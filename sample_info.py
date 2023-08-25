22aa99365b8eb17868fb0fb4daf9555cd75ec701
anonboy96 <118814879+anonboy96@users.noreply.github.com>
2023-08-25 18:21:44 +0530

    Add files via upload

---
+++ sample_info.py
@@ -0,0 +1,32 @@
+# Bot information
+SESSION = 'Media_search'
+USER_SESSION = 'User_Bot'
+API_ID = 29996504
+API_HASH = '0e8b98e48e6c0dfbdf9893879bb90e0a'
+BOT_TOKEN = '6450904676:AAGuZ6galyaybDJwdn5iZyrCVhUSVLvYMk8'
+USERBOT_STRING_SESSION = ''
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
+
+# Admins, Channels & Users
+ADMINS = [12345789, 'anonboy.96', 98765432]
+CHANNELS = [-10012345678, -100987654321, 'bombers']
+AUTH_USERS = []
+AUTH_CHANNEL = None
+
+# MongoDB information
+DATABASE_URI = "mongodb+srv://anonboy96:<password>@cluster0.9r9h1y6.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'Cluster0'
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
+
+# Messages
+START_MSG = """
+**Hi, I'm Media Search bot**
+
+Here you can search files in inline mode. Just press follwing buttons and start searching.
+"""
+
+SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
+INVITE_MSG = 'Please join @.... to use this bot'
